20b1607ab2c79d8f5aeea19df255a14e1e163fb5
nikoszz <19nikospap@gmail.com>
2021-05-15 10:18:36 +0300

    fix

--- src/main/java/com/nikoszz/playervaults/config/annotation/ConfigName.java
+++ src/main/java/com/nikoszz/playervaults/config/annotation/ConfigName.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package main.java.com.nikoszz.playervaults.config.annotation;
+package com.nikoszz.playervaults.config.annotation;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
